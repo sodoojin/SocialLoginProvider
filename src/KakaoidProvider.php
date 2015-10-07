1cfa2f4164f97ccf38456767475556acaf3b2acd
소두진 <programmer@programmer-PC>
2015-10-07 16:26:13 +0900

    patch

--- src/KakaoidProvider.php
+++ src/KakaoidProvider.php
@@ -1,117 +1,131 @@
 <?php
+
 namespace Visualplus\SocialLogin;
 
 use Laravel\Socialite\Two\AbstractProvider;
 use Laravel\Socialite\Two\ProviderInterface;
 use Laravel\Socialite\Two\User;
 
-class KakaoidProvider extends AbstractProvider implements ProviderInterface {
-	/**
-	 * XML -> array 형식 변환.
-	 * 
-	 * @param string
-	 * @return array
-	 */
-	private function parseXML($xml) {
-		$simpleXml = simplexml_load_string($xml, null, LIBXML_NOCDATA);
-		$json = json_encode($simpleXml);
+class KakaoidProvider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * XML -> array 형식 변환.
+     * 
+     * @param string
+     *
+     * @return array
+     */
+    private function parseXML($xml)
+    {
+        $simpleXml = simplexml_load_string($xml, null, LIBXML_NOCDATA);
+        $json = json_encode($simpleXml);
 
-		return json_decode($json, true);
-	}
-	
-	/**
+        return json_decode($json, true);
+    }
+
+    /**
      * Get the authentication URL for the provider.
      *
-     * @param  string  $state
+     * @param string $state
+     *
      * @return string
      */
-	protected function getAuthUrl($state) {
-		return $this->buildAuthUrlFromBase("https://kauth.kakao.com/oauth/authorize", $state);
-	}
-	
-	/**
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://kauth.kakao.com/oauth/authorize', $state);
+    }
+
+    /**
      * Get the token URL for the provider.
      *
      * @return string
      */
-	protected function getTokenUrl() {
-		return 'https://kauth.kakao.com/oauth/token';
-	}
-	
-	/**
+    protected function getTokenUrl()
+    {
+        return 'https://kauth.kakao.com/oauth/token';
+    }
+
+    /**
      * Get the access token for the given code.
      *
-     * @param  string  $code
+     * @param string $code
+     *
      * @return string
      */
-	public function getAccessToken($code) {
-		$response = $this->getHttpClient()->request('POST', $this->getTokenUrl(), [
-			'form_params'	=> $this->getTokenFields($code),
+    public function getAccessToken($code)
+    {
+        $response = $this->getHttpClient()->request('POST', $this->getTokenUrl(), [
+            'form_params'    => $this->getTokenFields($code),
         ]);
-		
+
         return $this->parseAccessToken($response->getBody());
-	}
-	
-	/**
+    }
+
+    /**
      * Get the POST fields for the token request.
      *
-     * @param  string  $code
+     * @param string $code
+     *
      * @return array
      */
-	protected function getTokenFields($code) {
-		return [
-			'grant_type' 	=> 'authorization_code',
-			'client_id'		=> $this->clientId,
-			'redirect_uri' 	=> $this->redirectUrl,
-			'code' 			=> $code,
-		];
-	}
-	
-	/**
+    protected function getTokenFields($code)
+    {
+        return [
+            'grant_type'       => 'authorization_code',
+            'client_id'        => $this->clientId,
+            'redirect_uri'     => $this->redirectUrl,
+            'code'             => $code,
+        ];
+    }
+
+    /**
      * Get the raw user for the given access token.
      *
-     * @param  string  $token
+     * @param string $token
+     *
      * @return array
      */
-	protected function getUserByToken($token) {
-		$response = $this->getHttpClient()->request('POST', 'https://kapi.kakao.com/v1/user/me', [
-			'headers' => [
-				'Authorization' => 'Bearer ' . $token,
-		    ],
-		]);
-		
-		return json_decode($response->getBody(), true);
-		
-	}
-	
-	/**
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->request('POST', 'https://kapi.kakao.com/v1/user/me', [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
      * Map the raw user array to a Socialite User instance.
      *
-     * @param  array  $user
+     * @param array $user
+     *
      * @return \Laravel\Socialite\User
      */
-	protected function mapUserToObject(array $user) {
-		
-		return (new User)->setRaw($user)->map([
-			'id'		=> $user['id'],
-			'properties' => [
-				'nickname'	=> $user['properties']['nickname'],
-				'thumbnail_image'	=> $user['properties']['thumbnail_image'],
-				'profile_image'		=> $user['properties']['profile_image'],
-			]
-		]);
-	}
-	
-	/**
-	 * User logout
-	 * 
-	 * @param string $user
-	 */
-	public function logOut($user) {
-		$response = $this->getHttpClient()->request('POST', 'https://kapi.kakao.com/v1/user/logout', [
-			'headers' => [
-				'Authorization' => 'Bearer '.$user->token
-			],
-		]);
-	}
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'         => $user['id'],
+            'properties' => [
+                'nickname'             => $user['properties']['nickname'],
+                'thumbnail_image'      => $user['properties']['thumbnail_image'],
+                'profile_image'        => $user['properties']['profile_image'],
+            ],
+        ]);
+    }
+
+    /**
+     * User logout.
+     * 
+     * @param string $user
+     */
+    public function logOut($user)
+    {
+        $response = $this->getHttpClient()->request('POST', 'https://kapi.kakao.com/v1/user/logout', [
+            'headers' => [
+                'Authorization' => 'Bearer '.$user->token,
+            ],
+        ]);
+    }
 }
--- src/SocialLoginExtendSocialite.php
+++ src/SocialLoginExtendSocialite.php
@@ -1,11 +1,14 @@
 <?php
+
 namespace Visualplus\SocialLogin;
 
 use SocialiteProviders\Manager\SocialiteWasCalled;
 
-class SocialLoginExtendSocialite {
-	public function handle(SocialiteWasCalled $socialiteWasCalled) {
-		$socialiteWasCalled->extendSocialite('naverid', \Visualplus\SocialLogin\NaveridProvider::class);
-		$socialiteWasCalled->extendSocialite('kakaoid', \Visualplus\SocialLogin\KakaoidProvider::class);
-	}
+class SocialLoginExtendSocialite
+{
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite('naverid', \Visualplus\SocialLogin\NaveridProvider::class);
+        $socialiteWasCalled->extendSocialite('kakaoid', \Visualplus\SocialLogin\KakaoidProvider::class);
+    }
 }
